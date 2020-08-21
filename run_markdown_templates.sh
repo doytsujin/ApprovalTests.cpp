98a5b34981302dc5299ae6ada64b62a88e34d388
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-21 21:04:25 +0100

    e   Revert to installing latest MarkdownSnippets.Tool

--- run_markdown_templates.sh
+++ run_markdown_templates.sh
@@ -11,8 +11,7 @@
 # To update:
 #   dotnet tool update  -g MarkdownSnippets.Tool
 
-# 20.0.0-beta.3
-dotnet tool update --version 20.0.0-beta.3 -g MarkdownSnippets.Tool
+dotnet tool update  -g MarkdownSnippets.Tool
 mdsnippets || exit 1
 
 echo "------------------------------------------"
